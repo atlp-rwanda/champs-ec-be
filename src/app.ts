cfd88cfee16d0b87c86469b42b17b4bb07c45f3a
PrinceRWIGIMBA <Princerwigimba07@gmail.com>
2024-03-26 20:28:46 +0200

    setting up swagger documentation

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,16 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import swaggerUi from "swagger-ui-express";
 import { Home } from "./utils/functions/redirect";
+import swaggerDocument from "../swagger.json";
 
 const app: express.Application = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(
   bodyParser.urlencoded({
