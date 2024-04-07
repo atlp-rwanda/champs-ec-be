5ca4f2dfb97f6bb04f1fcf5c47ea0e35f6548fa4
ISHIMWE Ami Paradis <141851169+amiparadis250@users.noreply.github.com>
2024-04-07 15:05:37 +0200

    fix(googleauth): Implement Google OAuth2 login for new users
    • Added Passport configuration for Google OAuth2 strategy.
    • Implemented route for handling Google OAuth2 callback.
    • Defined serialization and deserialization methods for Passport.
    • Created route to initiate Google OAuth2 login.
    • Returned token in response after successful authentication.
    • Enhanced user experience by enabling login via Google OAuth2.
    
    This feature allows users to login using Google.

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,77 +1,9 @@
+/* eslint-disable import/no-duplicates */
 import { Request, Response, NextFunction } from "express";
-import { Strategy as GoogleStrategy } from "passport-google-oauth2";
-import passport from "passport";
-
-import users from "../models/user";
-import { UserAttributes } from "../types/user.types";
+import passport from "../config/google.config";
 import { userToken } from "../utils/token.generator";
-
-interface UserModel extends UserAttributes {
-  id: string;
-  firstName: string;
-  lastName: string;
-  email: string;
-  profile?: string;
-  verified?: boolean;
-  createdAt?: Date;
-  updatedAt?: Date;
-  googleId?: string;
-  profileImage?: string;
-}
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      callbackURL: "/api/users/google/callback"
-    },
-    async (
-      accessToken: any,
-      refreshToken: any,
-      profile: {
-        emails: { value: string }[];
-        name: { givenName: string; familyName: string };
-        id: string;
-        photos: { value: string }[];
-      },
-      done: (arg0: unknown, arg1: UserModel | null) => void
-    ) => {
-      try {
-        const existingUser = await users.findOne({
-          where: { email: profile.emails?.[0].value }
-        });
-        if (existingUser) {
-          return done(null, existingUser);
-        }
-        const newUser: UserModel = await users.create({
-          firstName: profile.name?.givenName || "",
-          lastName: profile.name?.familyName || "",
-          email: profile.emails?.[0].value || "",
-          googleId: profile.id,
-          profileImage: profile.photos?.[0].value || "",
-          verified: true
-        });
-        done(null, newUser);
-      } catch (error) {
-        done(error, null);
-      }
-    }
-  )
-);
-
-passport.serializeUser((user: any, done) => {
-  done(null, user.id);
-});
-
-passport.deserializeUser(async (id: string, done) => {
-  try {
-    const user = await users.findByPk(id);
-    done(null, user);
-  } catch (error) {
-    done(error, null);
-  }
-});
+// eslint-disable-next-line import/no-duplicates
+import { UserModel } from "../config/google.config";
 
 const initiateGoogleLogin = (
   req: Request,
@@ -96,20 +28,22 @@ const handleGoogleCallback = async (req: Request, res: Response) => {
       return res.status(401).json({ error: "User not found" });
     }
 
-    const userObject = {
-      id: user.id,
-      email: user.email || "",
-      verified: user.verified || false
-    };
-    const id = userObject.id || "";
-    const email = userObject.email || "";
     try {
-      const token = await userToken(id, email);
-
-      res.json({ user: userObject, token });
+      const token = await userToken(user.id, user.email);
+      res.status(200).json({
+        status: "success",
+        message: "Login successful",
+        data: {
+          token
+        }
+      });
     } catch (error) {
-      return res.status(500).json({ error: "Failed to generate token" });
+      return res.status(500).json({
+        status: "not success",
+        error: "Login Failed, token generation failed"
+      });
     }
   })(req, res);
 };
-export { passport, initiateGoogleLogin, handleGoogleCallback };
+
+export { initiateGoogleLogin, handleGoogleCallback };
